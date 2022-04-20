58e301837b30b5f1a3563c5425b21e81567974f4
Hyunjinkyu <hyunjk0405@gmail.com>
2022-04-20 21:08:51 +0900

    spartacodingclub 2week practice Spring

--- src/main/java/com/sparta/springcore/model/User.java
+++ src/main/java/com/sparta/springcore/model/User.java
@@ -3,6 +3,7 @@ package com.sparta.springcore.model;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.*;
 
@@ -10,6 +11,7 @@ import javax.persistence.*;
 @Getter // get 함수를 일괄적으로 만들어줍니다.
 @NoArgsConstructor // 기본 생성자를 만들어줍니다.
 @Entity // DB 테이블 역할을 합니다.
+@ToString // toString()메서드 자동 생성
 public class User {
 
     // ID가 자동으로 생성 및 증가합니다.
